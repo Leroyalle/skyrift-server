083931f27643967b018a446a2488ced1bf53fb8a
Leroyalle <nikolajmelonov@yandex.ru>
2025-09-30 15:42:59 +0300

    feat(effect, seed): add find effect by id / type and seed stun effect

--- src/effect/effect.service.ts
+++ src/effect/effect.service.ts
@@ -2,6 +2,7 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Effect } from './entities/effect.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { EffectType } from 'src/common/enums/skill/effect-type.enum';
 
 @Injectable()
 export class EffectService implements OnModuleInit {
@@ -15,5 +16,21 @@ export class EffectService implements OnModuleInit {
   async onModuleInit() {
     const effects = await this.effectRepository.find();
     effects.forEach((e) => this.effectsMap.set(e.id, e));
+  }
+
+  public getEffectById(id: string): Effect | undefined {
+    return this.effectsMap.get(id);
+  }
+
+  public getEffectsByType(type: EffectType): Effect[] {
+    const effects: Effect[] = [];
+
+    this.effectsMap.forEach((e) => {
+      if (e.type === type) {
+        effects.push(e);
+      }
+    });
+
+    return effects;
   }
 }
