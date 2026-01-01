c814c46fbee132dafab34a1515ce811bde96a22b
Leroyalle <nikolajmelonov@yandex.ru>
2026-01-01 17:06:05 +0300

    feat(quest): add find by character id, find by id and mark as completed methods

--- src/quest/quest.service.ts
+++ src/quest/quest.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Quest } from './entities/quest.entity';
+import { Repository } from 'typeorm';
+import { PlayerQuest } from './entities/player-quest.entity';
 
 @Injectable()
-export class QuestService {}
+export class QuestService {
+  constructor(
+    @InjectRepository(Quest) private readonly questRepository: Repository<Quest>,
+    @InjectRepository(PlayerQuest) private readonly playerQuestRepository: Repository<PlayerQuest>,
+  ) {}
+
+  public async findAllByCharacterId(characterId: string) {
+    return await this.questRepository.find({
+      where: { playerQuests: { player: { id: characterId } } },
+    });
+  }
+
+  public async findById(id: string) {
+    return await this.questRepository.findOneBy({ id });
+  }
+
+  public async markAsCompleted(id: string) {
+    return await this.playerQuestRepository.update(id, { completedAt: new Date() });
+  }
+}
