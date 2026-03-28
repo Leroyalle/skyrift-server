c38e5c57ca70c2895321ed56e74f007a124229d1
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-28 19:38:03 +0300

    feat(character): add character orm entity

--- src/common/entities/actor-entity.entity.ts
+++ src/common/entities/actor-entity.entity.ts
@@ -1,5 +1,4 @@
-import { Equipment } from 'src/equipment/entities/equipment.entity';
-import { Column, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, JoinColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 import { Field, ID, Int, ObjectType } from '@nestjs/graphql';
 
@@ -7,103 +6,103 @@ import { Timestamp } from './timestamp.entity';
 
 class Appearance {
   @Column({ default: 'body_base' })
-  body: string;
+  body!: string;
 
   @Column({ default: 'head_base' })
-  head: string;
+  head!: string;
 }
 
 @ObjectType()
 export abstract class ActorEntity extends Timestamp {
   @PrimaryGeneratedColumn('uuid')
   @Field(() => ID)
-  id: string;
+  id!: string;
 
   @Column({ unique: true })
   @Field(() => String, { description: 'Имя' })
-  name: string;
+  name!: string;
 
   @Column({ default: 1 })
   @Field(() => Int, { description: 'Уровень', defaultValue: 1 })
-  level: number;
+  level!: number;
 
   @Column({ default: 300 })
   @Field(() => Int, {
     description: 'Максимальное здоровье',
     defaultValue: 300,
   })
-  maxHp: number;
+  maxHp!: number;
 
   @Column({ default: 300 })
   @Field(() => Int, {
     description: 'Текущее здоровье',
     defaultValue: 300,
   })
-  hp: number;
+  hp!: number;
 
   @Column({ default: 0 })
   @Field(() => Int, { description: 'Физический урон', defaultValue: 55 })
-  basePhysicalDamage: number;
+  basePhysicalDamage!: number;
 
   @Column({ default: 0 })
   @Field(() => Int, { description: 'Магический урон', defaultValue: 26 })
-  baseMagicDamage: number;
+  baseMagicDamage!: number;
 
   @Column({ default: 0 })
   @Field(() => Int, {
     description: 'Физическая защита (снижает физ. урон)',
     defaultValue: 0,
   })
-  physicalDefense: number;
+  physicalDefense!: number;
 
   @Column({ default: 0 })
   @Field(() => Int, {
     description: 'Магическая защита (снижает маг. урон)',
     defaultValue: 0,
   })
-  magicDefense: number;
+  magicDefense!: number;
 
   @Column({ default: 2.0 })
   @Field(() => Number, {
     description: 'Множитель крит. урона',
     defaultValue: 2.0,
   })
-  critMultiplier: number;
+  critMultiplier!: number;
 
   @Column({ default: 1.0 })
   @Field(() => Number, {
     description: 'Скорость атаки (уд/сек)',
     defaultValue: 1.0,
   })
-  attackSpeed: number;
+  attackSpeed!: number;
 
   @Column({ default: 1 })
   @Field(() => Int, {
     description: 'Дистанция атаки (в тайлах)',
     defaultValue: 1,
   })
-  attackRange: number;
+  attackRange!: number;
 
   @Column({ default: true })
   @Field(() => Boolean, { defaultValue: true })
-  isAlive: boolean;
+  isAlive!: boolean;
 
   @Column()
   @Field(() => Int, { description: 'X координата позиции игрока' })
-  x: number;
+  x!: number;
 
   @Column()
   @Field(() => Int, { description: 'Y координата позиции игрока' })
-  y: number;
+  y!: number;
 
   @Column({ default: 450 })
   @Field(() => Int, { defaultValue: 450 })
-  walkSpeed: number;
+  walkSpeed!: number;
 
-  @OneToOne(() => Equipment)
+  @Column()
   @JoinColumn({ name: 'equipmentId' })
-  equipment: Equipment;
+  equipmentId!: string;
 
   @Column(() => Appearance, { prefix: true })
-  appearance: Appearance;
+  appearance!: Appearance;
 }
---
+++ src/modules/character/infrastructure/persistence/character-orm.entity.ts
@@ -0,0 +1,57 @@
+import { CharacterClass } from 'src/character-class/entities/character-class.entity';
+import { ActorEntity } from 'src/common/entities/actor-entity.entity';
+import { PlayerQuest } from 'src/quest/entities/player-quest.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+@Entity()
+export class CharacterOrmEntity extends ActorEntity {
+  @Column({ default: 0 })
+  @Field(() => Int, { description: 'Опыт персонажа', defaultValue: 0 })
+  experience!: number;
+
+  @Column({ default: 100 })
+  @Field(() => Int, {
+    description: 'Опыт до следующего уровня',
+    defaultValue: 100,
+  })
+  experienceToNextLevel!: number;
+
+  @Column({ default: 0 })
+  @Field(() => Int, { description: 'Очки навыков', defaultValue: 0 })
+  skillPoints!: number;
+
+  @Column({ default: false })
+  @Field(() => Boolean, {
+    description: 'Статус удален или нет',
+    defaultValue: false,
+  })
+  isDeleted!: boolean;
+
+  @Column()
+  @Field(() => User, { description: 'Аккаунт пользователя' })
+  userid!: string;
+
+  @ManyToOne(() => CharacterClass, characterClass => characterClass.characters)
+  @Field(() => CharacterClass, { description: 'Класс персонажа' })
+  classId!: string;
+
+  @Column()
+  @Field({ description: 'Айди локации в которой находится игрок' })
+  locationId!: string;
+
+  @Column({ type: 'array' })
+  @Field({ description: 'Навыки персонаж' })
+  skillsIds!: string[];
+
+  @Column()
+  @Field()
+  bagId!: string;
+
+  @Column({ type: 'array' })
+  @Field(() => [PlayerQuest])
+  questsIds!: string[];
+}
