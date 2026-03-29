0db0001886fc13d28428475aecaf454b1aa093b9
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-30 00:03:33 +0300

    feat(quest): add quest orm entity

---
+++ src/modules/quest/infrastructure/persistence/entities/quest-orm.entity.ts
@@ -0,0 +1,47 @@
+import { Npc } from 'src/characters/npc/entities/npc.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+import { Field, ID, Int, ObjectType } from '@nestjs/graphql';
+
+import { QuestPrerequisite } from '../../../domain/types/prerequisites.type';
+import { QuestStep } from '../../../domain/types/quest-step.type';
+
+@ObjectType()
+@Entity('quests')
+export class Quest {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id!: string;
+
+  @Column()
+  @Field()
+  name!: string;
+
+  @Column()
+  @Field()
+  description!: string;
+
+  @Column()
+  @Field(() => Int)
+  expReward!: number;
+
+  @Column()
+  @Field(() => Int)
+  goldReward!: number;
+
+  @Column({ type: 'jsonb', nullable: true })
+  itemRewards!: {
+    templateId: string;
+    quantity: number;
+  }[];
+
+  @Column({ type: 'jsonb' })
+  steps!: QuestStep[];
+
+  @Column({ type: 'jsonb', nullable: true })
+  prerequisites!: QuestPrerequisite[] | null;
+
+  @Column()
+  @Field(() => Npc)
+  giverNpcId!: string;
+}
