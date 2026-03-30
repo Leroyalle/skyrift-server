3de9660b86af318685ede4221d748381479ae524
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-30 14:26:56 +0300

    feat(quest): implement find active or completed quests in memory player quest repo

--- src/realtime/quest/domain/ports/in-memory-player-quest-repository.port.ts
+++ src/realtime/quest/domain/ports/in-memory-player-quest-repository.port.ts
@@ -2,6 +2,8 @@ import type { PlayerQuest } from 'src/modules/quest/domain/entities/player-quest
 
 export interface InMemoryPlayerQuestRepositoryPort {
   getById(id: string): PlayerQuest | undefined;
-  save(quest: PlayerQuest): void;
-  remove(id: string): void;
+  save(quest: PlayerQuest, characterId: string): void;
+  remove(id: string, characterId: string): void;
+  findActiveByCharacterId(characterId: string): PlayerQuest[];
+  findCompletedIdsByCharacterId(characterId: string): PlayerQuest[];
 }
--- src/realtime/quest/infrastructure/repositories/in-memory-player-quest.repository.ts
+++ src/realtime/quest/infrastructure/repositories/in-memory-player-quest.repository.ts
@@ -3,17 +3,48 @@ import type { PlayerQuest } from 'src/modules/quest/domain/entities/player-quest
 import type { InMemoryPlayerQuestRepositoryPort } from '../../domain/ports/in-memory-player-quest-repository.port';
 
 export class InMemoryPlayerQuestRepository implements InMemoryPlayerQuestRepositoryPort {
-  private readonly quests: Map<string, PlayerQuest> = new Map();
+  private readonly questById: Map<string, PlayerQuest> = new Map();
+  private readonly questsByCharacterId: Map<string, Set<string>> = new Map();
 
   public getById(id: string) {
-    return this.quests.get(id);
+    return this.questById.get(id);
   }
 
-  public save(quest: PlayerQuest) {
-    this.quests.set(quest.id, quest);
+  public save(quest: PlayerQuest, characterId: string) {
+    this.questById.set(quest.id, quest);
+
+    let characterQuests = this.questsByCharacterId.get(characterId);
+    if (!characterQuests) {
+      characterQuests = new Set();
+      this.questsByCharacterId.set(characterId, characterQuests);
+    }
+    characterQuests.add(quest.id);
   }
 
-  public remove(id: string) {
-    this.quests.delete(id);
+  public remove(id: string, characterId: string) {
+    this.questById.delete(id);
+    const characterQuests = this.questsByCharacterId.get(characterId);
+    if (characterQuests) {
+      characterQuests.delete(id);
+    }
+  }
+
+  private getAllByCharacterId(characterId: string, status: 'active' | 'completed') {
+    const questIds = this.questsByCharacterId.get(characterId);
+    if (!questIds) return [];
+    return Array.from(questIds)
+      .map(id => this.questById.get(id))
+      .filter(
+        (quest): quest is PlayerQuest =>
+          !!quest && (status === 'completed' ? !!quest.completedAt : !quest.completedAt),
+      );
+  }
+
+  public findActiveByCharacterId(characterId: string) {
+    return this.getAllByCharacterId(characterId, 'active');
+  }
+
+  public findCompletedIdsByCharacterId(characterId: string) {
+    return this.getAllByCharacterId(characterId, 'completed');
   }
 }
