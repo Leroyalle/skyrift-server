0e5ce4354382f61ebb0fd9db6e04409be83d4769
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-01 00:56:16 +0300

    feat(mob-session): implement get mob session snapshot by mob id query

--- src/realtime/mob-session/application/facades/mob-session.facade.ts
+++ src/realtime/mob-session/application/facades/mob-session.facade.ts
@@ -1,16 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 
-import type { InMemoryMobSessionRepositoryPort } from '../../domain/ports/in-memory-mob-session-repository.port';
+import type { MobSessionRepositoryPort } from '../../domain/ports/in-memory-mob-session-repository.port';
 import { MOB_SESSION_REPOSITORY_TOKEN } from '../ports/tokens';
 
 @Injectable()
 export class MobSessionFacade {
   constructor(
     @Inject(MOB_SESSION_REPOSITORY_TOKEN)
-    private readonly mobSessionRepository: InMemoryMobSessionRepositoryPort,
+    private readonly mobSessionRepository: MobSessionRepositoryPort,
   ) {}
 
   public findById(id: string) {
-    return this.mobSessionRepository.findById(id);
+    return this.mobSessionRepository.findByMobId(id);
   }
 }
---
+++ src/realtime/mob-session/index.ts
@@ -0,0 +1,2 @@
+export { GetMobSessionSnapshotByMobIdPort } from './application/ports/get-mob-session-snapshot-by-mob-id.port';
+export { GET_MOB_SESSION_SNAPSHOT_BY_ID_PORT } from './application/ports/tokens';
