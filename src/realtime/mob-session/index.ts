f73090c5d9a637e83164fc43e34c1d681df3f018
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-02 11:45:07 +0300

    feat(contracts): add entity session type

--- src/realtime/mob-session/index.ts
+++ src/realtime/mob-session/index.ts
@@ -1,2 +1,3 @@
 export { GetMobSessionSnapshotByMobIdPort } from './application/ports/get-mob-session-snapshot-by-mob-id.port';
 export { GET_MOB_SESSION_SNAPSHOT_BY_ID_PORT } from './application/ports/tokens';
+export { type MobSessionSnapshot } from './domain/types/mob-session.type';
---
+++ src/realtime/player-session/index.ts
@@ -0,0 +1,3 @@
+export { type GetPlayerSessionSnapshotByCharacterIdPort } from './application/ports/get-player-session-snapshot-by-character-id.port';
+export { GET_PLAYER_SESSION_SNAPSHOT_BY_CHARACTER_ID_PORT } from './application/ports/tokens';
+export { type PlayerSessionSnapshot } from './domain/types/player-session.type';
