21564525513d75ea94402494354e0459beda2a2a
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-02 23:45:53 +0300

    feat(skill): implement persistence repository

---
+++ src/modules/skill/domain/ports/skill-persistence-repository.port.ts
@@ -0,0 +1,7 @@
+import type { Skill } from '../entities/skill.entity';
+
+export interface SkillPersistenceRepositoryPort {
+  save(skill: Skill): Promise<Skill>;
+  remove(id: string): Promise<void>;
+  findById(id: string): Promise<Skill | null>;
+}
---
+++ src/modules/skill/infrastructure/persistence/repositories/skill-persistence.repository.ts
@@ -0,0 +1,31 @@
+import type { Skill } from 'src/modules/skill/domain/entities/skill.entity';
+import type { SkillPersistenceRepositoryPort } from 'src/modules/skill/domain/ports/skill-persistence-repository.port';
+import type { Repository } from 'typeorm';
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { SkillOrmEntity } from '../entities/skill-orm.entity';
+import { SkillMapper } from '../mappers/skill.mapper';
+
+@Injectable()
+export class SkillPersistenceRepository implements SkillPersistenceRepositoryPort {
+  constructor(
+    @InjectRepository(SkillOrmEntity) private readonly skillRepository: Repository<SkillOrmEntity>,
+  ) {}
+
+  public async save(skill: Skill): Promise<Skill> {
+    const persistence = SkillMapper.toPersistence(skill);
+    const result = await this.skillRepository.save(persistence);
+    return SkillMapper.toDomain(result);
+  }
+
+  public async remove(id: string): Promise<void> {
+    await this.skillRepository.delete(id);
+  }
+
+  public async findById(id: string): Promise<Skill | null> {
+    const persistence = await this.skillRepository.findOneBy({ id });
+    return persistence ? SkillMapper.toDomain(persistence) : null;
+  }
+}
