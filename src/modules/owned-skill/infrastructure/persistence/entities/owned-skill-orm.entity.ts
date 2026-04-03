2a29abbd479d2c8f9af3faa865c4e6e2b7dbe5c1
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-03 12:59:28 +0300

    feat(owned-skill): add orm entity

---
+++ src/modules/owned-skill/infrastructure/persistence/entities/owned-skill-orm.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@Entity()
+@Unique('UQ_owned_skill_owner_skill', ['ownerKey', 'ownerId', 'skillId'])
+@ObjectType()
+export class OwnedSkillOrmEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Field()
+  id!: string;
+
+  @Column()
+  @Field({ description: 'Навык' })
+  skillId!: string;
+
+  @Column()
+  ownerId!: string;
+
+  @Column()
+  ownerType!: 'player' | 'npc' | 'mob';
+
+  @Column({ type: 'bigint', nullable: true })
+  @Field({ nullable: true, description: 'Время последнего использования' })
+  lastUsedAt!: number;
+
+  @Column({ type: 'bigint', nullable: true })
+  @Field({ nullable: true, description: 'Время окончания кулдауна' })
+  cooldownEnd!: number;
+
+  @Column({ type: 'int', default: 1 })
+  @Field(() => Int, { description: 'Уровень навыка' })
+  level!: number;
+}
