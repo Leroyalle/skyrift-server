bf90fed958c12ef72b38be9e52f68fcea356d213
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-03 10:47:28 +0300

    feat(effect): add infra mapper

---
+++ src/modules/effect/infrastructure/persistence/mappers/effect.mapper.ts
@@ -0,0 +1,31 @@
+import { Effect } from 'src/modules/effect/domain/entities/effect.entity';
+
+import type { EffectOrmEntity } from '../entities/effect-orm.entity';
+
+export class EffectMapper {
+  public static toDomain(payload: EffectOrmEntity): Effect {
+    return Effect.create({
+      durationMs: payload.durationMs,
+      id: payload.id,
+      skillId: payload.skillId,
+      type: payload.type,
+      amount: payload.amount,
+      slowPercent: payload.slowPercent,
+      damagePerSecond: payload.damagePerSecond,
+    });
+  }
+
+  public static toPersistence(payload: Effect): EffectOrmEntity {
+    const snapshot = payload.snapshot();
+
+    return {
+      slowPercent: snapshot.slowPercent,
+      amount: snapshot.amount,
+      damagePerSecond: snapshot.damagePerSecond,
+      durationMs: snapshot.durationMs,
+      id: snapshot.id,
+      skillId: snapshot.skillId,
+      type: snapshot.type,
+    };
+  }
+}
