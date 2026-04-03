b9fc45c928f7177cda61b07ed108eabd3ad2dc42
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-03 18:47:55 +0300

    feat(effect): add type

---
+++ src/realtime/effect/domain/types/effect.type.ts
@@ -0,0 +1,17 @@
+export interface IEffect {
+  type: EffectType;
+  damagePerSecond?: number;
+  durationMs: number;
+  amount?: number;
+  slowPercent?: number;
+  skillId: string;
+}
+
+export type EffectType =
+  | 'instant_damage'
+  | 'damage_over_time'
+  | 'heal'
+  | 'shield'
+  | 'speed_boost'
+  | 'slow'
+  | 'stun';
