28aaf7dcba986f348fb33b69c8c64fd3dc3abd28
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-03 12:59:54 +0300

    feat(owned-skill): implement persistence repository

---
+++ src/modules/owned-skill/domain/ports/owned-skill-persistence-repository.port.ts
@@ -0,0 +1,9 @@
+import type { OwnedSkill } from '../entities/owned-skill.entity';
+
+export interface OwnedSkillPersistenceRepositoryPort {
+  save(domain: OwnedSkill): Promise<void>;
+  remove(id: OwnedSkill['id']): Promise<void>;
+  findBySkillId(skillId: OwnedSkill['skillId']): Promise<OwnedSkill[]>;
+  findById(id: OwnedSkill['id']): Promise<OwnedSkill | null>;
+  update(domain: OwnedSkill): Promise<void>;
+}
---
+++ src/modules/owned-skill/infrastructure/persistence/repositories/owned-skill-persistence.repository.ts
@@ -0,0 +1,39 @@
+import type { OwnedSkill } from 'src/modules/owned-skill/domain/entities/owned-skill.entity';
+import type { OwnedSkillPersistenceRepositoryPort } from 'src/modules/owned-skill/domain/ports/owned-skill-persistence-repository.port';
+import type { Repository } from 'typeorm';
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { OwnedSkillOrmEntity } from '../entities/owned-skill-orm.entity';
+import { OwnedSkillMapper } from '../mappers/owned-skill.mapper';
+
+@Injectable()
+export class OwnedSkillPersistenceRepository implements OwnedSkillPersistenceRepositoryPort {
+  constructor(
+    @InjectRepository(OwnedSkillOrmEntity)
+    private readonly repository: Repository<OwnedSkillOrmEntity>,
+  ) {}
+
+  public async save(domain: OwnedSkill): Promise<void> {
+    await this.repository.save(OwnedSkillMapper.toPersistence(domain));
+  }
+
+  public async findById(id: OwnedSkill['id']): Promise<OwnedSkill | null> {
+    const result = await this.repository.findOneBy({ id });
+    return result ? OwnedSkillMapper.toDomain(result) : null;
+  }
+
+  public async remove(id: OwnedSkill['id']): Promise<void> {
+    await this.repository.delete(id);
+  }
+
+  public async update(domain: OwnedSkill): Promise<void> {
+    await this.repository.save(OwnedSkillMapper.toPersistence(domain));
+  }
+
+  public async findBySkillId(skillId: OwnedSkill['skillId']): Promise<OwnedSkill[]> {
+    const result = await this.repository.find({ where: { skillId } });
+    return result.map(OwnedSkillMapper.toDomain);
+  }
+}
