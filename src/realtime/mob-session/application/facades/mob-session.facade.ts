e0c51e043b1cfc75fefb6d7dd3ff48ff4098cd44
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-05 13:57:40 +0300

    feat(mob-session): implement applyDamage facade method

--- src/realtime/mob-session/application/facades/mob-session.facade.ts
+++ src/realtime/mob-session/application/facades/mob-session.facade.ts
@@ -1,8 +1,10 @@
+import type { IEntityRef } from 'src/realtime/shared/types/entity-ref.type';
 import type { IPositionTile } from 'src/realtime/shared/types/position.type';
 
 import { Inject, Injectable } from '@nestjs/common';
 
 import type { MobSessionRepositoryPort } from '../../domain/ports/in-memory-mob-session-repository.port';
+import type { IReceiveDamageResult } from '../../domain/types/receive-damage-result.type';
 import type { MobSessionFacadePort } from '../ports/mob-session-facade.port';
 import { MOB_SESSION_REPOSITORY_TOKEN } from '../ports/tokens';
 
@@ -14,13 +16,32 @@ export class MobSessionFacade implements MobSessionFacadePort {
   ) {}
 
   public findById(id: string) {
-    return this.mobSessionRepository.findByMobId(id);
+    return this.mobSessionRepository.findById(id);
   }
 
   public move(mobId: string, position: IPositionTile, now: number): void {
-    const mobSession = this.mobSessionRepository.findByMobId(mobId);
+    const mobSession = this.mobSessionRepository.findById(mobId);
     if (!mobSession) return;
     mobSession.moveTo(position.x, position.y, now);
     this.mobSessionRepository.save(mobSession);
+  }
+
+  public applyDamage(
+    id: string,
+    amount: number,
+    attackerRef: IEntityRef,
+  ): IReceiveDamageResult | undefined {
+    const session = this.mobSessionRepository.findById(id);
+    if (!session) return;
+
+    const result = session.receiveDamage(amount);
+
+    if (result.isAlive) {
+      session.updateAggro(attackerRef, amount);
+    }
+
+    this.mobSessionRepository.save(session);
+
+    return result;
   }
 }
--- src/realtime/mob-session/application/ports/mob-session-facade.port.ts
+++ src/realtime/mob-session/application/ports/mob-session-facade.port.ts
@@ -1,5 +1,14 @@
+import type { IEntityRef } from 'src/realtime/shared/types/entity-ref.type';
 import type { IPositionTile } from 'src/realtime/shared/types/position.type';
+
+import type { IReceiveDamageResult } from '../../domain/types/receive-damage-result.type';
 
 export interface MobSessionFacadePort {
   move(mobId: string, position: IPositionTile, now: number): void;
+  applyDamage(
+    id: string,
+    amount: number,
+    attackerRef: IEntityRef,
+  ): IReceiveDamageResult | undefined;
+  // updateAggro(id: string, entityRef: IEntityRef, amount: number): void;
 }
