3f53a843b35f04b8a6ef29d56b6e4a3f6a166d19
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-06 14:53:04 +0300

    feat(combat): improve target type by mapping

--- src/realtime/combat/application/ports/combat-action-planner.port.ts
+++ src/realtime/combat/application/ports/combat-action-planner.port.ts
@@ -1,20 +1,27 @@
 import type { IEntityRef } from 'src/realtime/shared/types/entity-ref.type';
+import type { IPositionTile } from 'src/realtime/shared/types/position.type';
 
 export interface CombatActionPlannerPayload {
   attackerRef: IEntityRef;
-  target: Target;
+  target: CombatTarget;
   skillId: string | null;
 }
-type Target =
-  | {
-      kind: 'target';
-      victimRef: IEntityRef;
-    }
-  | {
-      kind: 'aoe';
-      x: number;
-      y: number;
-    };
+
+type TargetKind = 'aoe' | 'target';
+
+type TargetValueByKind = {
+  aoe: IPositionTile;
+  target: IEntityRef;
+};
+
+type Target<K extends TargetKind = TargetKind> = {
+  kind: K;
+  value: TargetValueByKind[K];
+};
+
+type CombatTarget = {
+  [K in TargetKind]: Target<K>;
+}[TargetKind];
 
 export interface CombatActionPlannerPort {
   execute(payload: CombatActionPlannerPayload): Promise<void>;
