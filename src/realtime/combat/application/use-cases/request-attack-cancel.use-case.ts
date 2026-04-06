5d0d5689560bafe1a775649bd58ae3e1549b3f55
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-06 17:05:04 +0300

    feat(combat): implement request attack cancel use case

---
+++ src/realtime/combat/application/use-cases/request-attack-cancel.use-case.ts
@@ -0,0 +1,35 @@
+import {
+  ENTITY_ACTION_FACADE_TOKEN,
+  ENTITY_RESOLVER_TOKEN,
+  type EntityActionFacadePort,
+  type EntityResolverPort,
+} from 'src/realtime/entity-registry';
+import type { IEntityRef } from 'src/realtime/shared/types/entity-ref.type';
+
+import { Inject, Injectable } from '@nestjs/common';
+
+import type { ActionQueueRepositoryPort } from '../../domain/ports/action-queue-repository.port';
+import { ACTION_QUEUE_REPOSITORY_TOKEN } from '../ports/tokens';
+
+interface Payload {
+  entityRef: IEntityRef;
+}
+
+@Injectable()
+export class RequestAttackCancelUseCase {
+  constructor(
+    @Inject(ACTION_QUEUE_REPOSITORY_TOKEN)
+    private readonly actionQueueRepository: ActionQueueRepositoryPort,
+    @Inject(ENTITY_RESOLVER_TOKEN) private readonly entityResolver: EntityResolverPort,
+    @Inject(ENTITY_ACTION_FACADE_TOKEN) private readonly entityActionFacade: EntityActionFacadePort,
+  ) {}
+
+  public execute(payload: Payload) {
+    const attacker = this.entityResolver.getByRef(payload.entityRef);
+
+    if (!attacker) return;
+
+    this.entityActionFacade.cancelAttack(attacker);
+    this.actionQueueRepository.clear(payload.entityRef);
+  }
+}
