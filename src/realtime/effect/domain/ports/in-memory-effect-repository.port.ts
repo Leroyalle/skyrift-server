b86cf72d73ab7508d52310438222fd082c3c04ce
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-09 19:45:55 +0300

    feat(effect): improve repository methods, implement getAll

--- src/realtime/effect/domain/ports/in-memory-effect-repository.port.ts
+++ src/realtime/effect/domain/ports/in-memory-effect-repository.port.ts
@@ -1,9 +1,11 @@
-import type { IEntityKey, IEntityRef } from 'src/realtime/shared/types/entity-ref.type';
+import type { IEntityRef } from 'src/realtime/shared/types/entity-ref.type';
 
-import type { IEffect } from '../types/effect.type';
+import type { EffectType, IEffect } from '../types/effect.type';
 
 export interface InMemoryEffectRepositoryPort {
-  set(entityRef: IEntityRef, effect: IEffect): void;
-  get(id: IEntityKey): IEffect | null;
-  remove(id: IEntityKey): void;
+  add(entityRef: IEntityRef, effect: IEffect): void;
+  get(ref: IEntityRef, type: EffectType): IEffect[] | null;
+  remove(ref: IEntityRef): void;
+  getAll(): Iterable<{ entityRef: IEntityRef; type: EffectType; effects: IEffect[] }>;
+  set(entityRef: IEntityRef, map: Map<EffectType, IEffect[]>): void;
 }
