1418409de2728d92fc4920c22ba7a1dc26890a80
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-10 01:23:27 +0300

    feat(spawn): add spawn orm entity

---
+++ src/modules/spawn/infrastructure/persistence/entities/entity-spawn-orm.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+import { Field, ID, Int, ObjectType } from '@nestjs/graphql';
+
+@Entity()
+@ObjectType()
+export class EntitySpawnOrmEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id!: string;
+
+  @Column()
+  @Field(() => Int)
+  spawnX!: number;
+
+  @Column()
+  @Field(() => Int)
+  spawnY!: number;
+
+  @Column()
+  @Field(() => Int)
+  areaRadius!: number;
+
+  @Column()
+  @Field()
+  locationId!: string;
+}
