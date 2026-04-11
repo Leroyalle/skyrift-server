7dc1e6dd6ef75fdc03fb150cc092db5b8c9c1651
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-11 18:10:33 +0300

    feat(character): add domain type

---
+++ src/modules/character/domain/types/character.type.ts
@@ -0,0 +1,40 @@
+import type { Appearance } from 'src/common/domain/vo/appearance.vo';
+
+export interface ICharacter {
+  experience: number;
+  experienceToNextLevel: number;
+  skillPoints: number;
+  isDeleted: boolean;
+  userId: string;
+  classId: string;
+  skillsIds: string[];
+  locationId: string;
+  id: string;
+  name: string;
+  level: number;
+  maxHp: number;
+  hp: number;
+  basePhysicalDamage: number;
+  baseMagicDamage: number;
+  physicalDefense: number;
+  magicDefense: number;
+  critMultiplier: number;
+  attackSpeed: number;
+  attackRange: number;
+  isAlive: boolean;
+  questsIds: string[];
+  x: number;
+  y: number;
+  walkSpeed: number;
+  equipmentId: string;
+  appearance: Appearance;
+  createdAt: Date;
+  bagId: string;
+}
+
+export type CharacterSnapshot = Omit<ICharacter, 'appearance'> & {
+  appearance: {
+    body: string;
+    head: string;
+  };
+};
