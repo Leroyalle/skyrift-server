f0f1c8c77d31207777924da232d5d9966ac5dc2a
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-13 16:21:45 +0300

    feat(equipment): add domain type

---
+++ src/modules/equipment/domain/types/equipment.type.ts
@@ -0,0 +1,13 @@
+export interface IEquipment {
+  id: string;
+  characterId: string;
+  helmetId: string | null;
+  breastplateId: string | null;
+  glovesId: string | null;
+  legsId: string | null;
+  mainHandId: string | null;
+  offHandId: string | null;
+  ring1Id: string | null;
+  ring2Id: string | null;
+  cloakId: string | null;
+}
