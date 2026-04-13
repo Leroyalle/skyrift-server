62832545cb461a4fe58a260c099708bd99155e7a
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-13 16:16:23 +0300

    feat(character): implement findCharacterById query handler

---
+++ src/modules/character/application/queries/find-character-by-id/find-character-by-id.handler.ts
@@ -0,0 +1,20 @@
+import type { CharacterRepositoryPort } from 'src/modules/character/domain/ports/character-repository.port';
+
+import { Inject } from '@nestjs/common';
+import { type IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+
+import { CHARACTER_REPOSITORY_TOKEN } from '../../ports/tokens';
+
+import { FindCharacterByIdQuery } from './find-character-by-id.query';
+
+@QueryHandler(FindCharacterByIdQuery)
+export class FindCharacterByIdHandler implements IQueryHandler<FindCharacterByIdQuery> {
+  constructor(
+    @Inject(CHARACTER_REPOSITORY_TOKEN)
+    private readonly characterRepository: CharacterRepositoryPort,
+  ) {}
+
+  public execute(query: FindCharacterByIdQuery) {
+    return this.characterRepository.findOwnedCharacter(query.props.userId, query.props.characterId);
+  }
+}
