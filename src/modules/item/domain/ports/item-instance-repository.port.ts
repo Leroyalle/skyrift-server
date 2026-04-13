436dbd7e6fcab2e680a07c7ae49d5f8fc3a13bcd
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-13 16:26:45 +0300

    feat(item): implement findByIds repository method

--- src/modules/item/domain/ports/item-instance-repository.port.ts
+++ src/modules/item/domain/ports/item-instance-repository.port.ts
@@ -1,8 +1,16 @@
 import type { ItemInstance } from '../types/item-instance.type';
 
 export interface ItemInstanceRepositoryPort {
-  findById(id: string): Promise<ItemInstance | null>;
-  findByTemplateId(templateId: string): Promise<ItemInstance[]>;
+  findById(id: ItemInstance['id']): Promise<ItemInstance | null>;
+  findByIds(ids: ItemInstance['id'][]): Promise<ItemInstance[]>;
   save(itemInstance: ItemInstance): Promise<void>;
   delete(itemInstance: ItemInstance): Promise<void>;
+  findByOwner(
+    ownerId: ItemInstance['ownerId'],
+    ownerType: ItemInstance['ownerType'],
+  ): Promise<ItemInstance[]>;
+  findByContainer(
+    containerId: ItemInstance['containerId'],
+    containerType: ItemInstance['containerType'],
+  ): Promise<ItemInstance[]>;
 }
--- src/modules/item/domain/ports/item-tempalte-repository.port.ts
+++ src/modules/item/domain/ports/item-tempalte-repository.port.ts
@@ -1,7 +1,8 @@
-import type { ItemTemplate } from '../types/item-tamplate.type';
+import type { ItemTemplate } from '../types/item-template.type';
 
 export interface ItemTemplateRepositoryPort {
   find(id: ItemTemplate['id']): Promise<ItemTemplate | null>;
+  findByIds(ids: ItemTemplate['id'][]): Promise<ItemTemplate[]>;
   findAll(): Promise<ItemTemplate[]>;
   save(itemTemplate: ItemTemplate): Promise<void>;
   delete(itemTemplate: ItemTemplate): Promise<void>;
