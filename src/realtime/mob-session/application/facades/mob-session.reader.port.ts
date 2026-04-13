fa926ebcc2879e81b86ee44687433f0a1b0a6a24
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-13 18:11:13 +0300

    feat(mob-session): implement mob session reader

---
+++ src/realtime/mob-session/application/facades/mob-session.reader.port.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import type { MobSessionRepositoryPort } from '../../domain/ports/in-memory-mob-session-repository.port';
+import type { MobSessionReaderPort } from '../ports/mob-session-reader.port';
+import { MOB_SESSION_REPOSITORY_TOKEN } from '../ports/tokens';
+
+@Injectable()
+export class MobSessionReader implements MobSessionReaderPort {
+  constructor(
+    @Inject(MOB_SESSION_REPOSITORY_TOKEN)
+    private readonly mobSessionRepository: MobSessionRepositoryPort,
+  ) {}
+
+  public getByLocationId(locationId: string) {
+    const mobSessions = this.mobSessionRepository.getByLocationId(locationId);
+    return mobSessions.map(session => session.toPublicSnapshot());
+  }
+}
--- src/realtime/mob-session/index.ts
+++ src/realtime/mob-session/index.ts
@@ -1,3 +1,7 @@
 export { GetMobSessionSnapshotByMobIdPort } from './application/ports/get-mob-session-snapshot-by-mob-id.port';
-export { GET_MOB_SESSION_SNAPSHOT_BY_ID_PORT } from './application/ports/tokens';
+export {
+  GET_MOB_SESSION_SNAPSHOT_BY_ID_PORT,
+  MOB_SESSION_READER_TOKEN,
+} from './application/ports/tokens';
 export { type MobSessionSnapshot, type MobStateStats } from './domain/types/mob-session.type';
+export { MobSessionReaderPort } from './application/ports/mob-session-reader.port';
