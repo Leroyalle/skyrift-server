e8510d6bb8ef3dd90043c07dad27c3dabf8423a2
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-15 11:47:29 +0300

    feat(combat): add request use skill use case port and token

--- src/realtime/combat/index.ts
+++ src/realtime/combat/index.ts
@@ -4,6 +4,8 @@ export {
   AOE_ZONE_READER_TOKEN,
   REQUEST_ATTACK_MOVE_USE_CASE_TOKEN,
   REQUEST_ATTACK_CANCEL_USE_CASE_TOKEN,
+  REQUEST_USE_SKILL_USE_CASE_TOKEN,
 } from './application/ports/tokens';
 export { RequestAttackMoveUseCasePort } from './application/ports/request-attack-move-use-case.port';
 export { RequestAttackCancelUseCasePort } from './application/ports/request-attack-cancel-use-case.port';
+export { RequestUseSkillUseCasePort } from './application/ports/request-use-skill-use-case.port';
