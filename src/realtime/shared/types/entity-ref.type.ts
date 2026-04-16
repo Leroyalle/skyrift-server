a7ca63e82d99f8863bd90adcd94b4f8edef4fdee
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-16 16:09:13 +0300

    feat(entity-ref): add support type generics

--- src/realtime/shared/types/entity-ref.type.ts
+++ src/realtime/shared/types/entity-ref.type.ts
@@ -1,7 +1,7 @@
 export type IEntityType = 'player' | 'npc' | 'mob';
 export type IEntityKey<T extends IEntityType = IEntityType> = `${T}_${string}`;
 
-export interface IEntityRef {
+export interface IEntityRef<T extends IEntityType = IEntityType> {
   id: string;
-  type: IEntityType;
+  type: T;
 }
