c63f592aff5f2cd7114f6844b9ea89514bd2ec06
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-18 11:05:25 +0300

    feat(flow): implement build player location state use case

---
+++ src/realtime/flow/application/use-cases/build-player-location-state.use-case.ts
@@ -0,0 +1,57 @@
+import {
+  BAG_CONTAINER_READER_TOKEN,
+  type BagContainerReaderPort,
+  EQUIPMENT_CONTAINER_FACADE_TOKEN,
+  type EquipmentContainerFacadePort,
+} from 'src/realtime/container';
+import type { GameInitialData } from 'src/realtime/contracts/types/game-initial-data.type';
+import { ENTITY_RESOLVER_TOKEN, type EntityResolverPort } from 'src/realtime/entity-registry';
+import { SessionClientMapper } from 'src/realtime/shared/mappers/session-client.mapper';
+
+import { Inject, Injectable } from '@nestjs/common';
+
+import type { BuildLocationWorldStatePort } from '../ports/build-location-world-state-use-case.port';
+import { BUILD_LOCATION_WORLD_STATE_USE_CASE_TOKEN } from '../ports/tokens';
+
+@Injectable()
+export class BuildPlayerLocationStateUseCase {
+  constructor(
+    @Inject(BUILD_LOCATION_WORLD_STATE_USE_CASE_TOKEN)
+    private readonly buildLocationWorldStateUseCase: BuildLocationWorldStatePort,
+    @Inject(ENTITY_RESOLVER_TOKEN) private readonly entityResolver: EntityResolverPort,
+    @Inject(BAG_CONTAINER_READER_TOKEN) private readonly bagContainerReader: BagContainerReaderPort,
+    @Inject(EQUIPMENT_CONTAINER_FACADE_TOKEN)
+    private readonly equipmentFacade: EquipmentContainerFacadePort,
+  ) {}
+
+  public async execute(
+    characterId: string,
+    locationId: string,
+  ): Promise<GameInitialData | undefined> {
+    const locationResult = this.buildLocationWorldStateUseCase.execute(locationId);
+
+    const player = this.entityResolver.getByRef({
+      type: 'player',
+      id: characterId,
+    });
+
+    if (!player) return;
+
+    const playerSession = SessionClientMapper.mapPlayerSession(player);
+
+    const bag = this.bagContainerReader.findById(player.bagId);
+
+    if (!bag) return;
+
+    const equipment = await this.equipmentFacade.getContainerById(player.equipmentId);
+
+    if (!equipment) return;
+
+    return {
+      ...locationResult,
+      player: playerSession,
+      bag,
+      equipment,
+    };
+  }
+}
