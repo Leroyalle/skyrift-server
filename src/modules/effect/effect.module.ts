18dd367f42ec2fcfc0500f5fbfa55004b3138f2c
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-24 23:45:26 +0300

    feat(effect): implement module and register providers

--- src/modules/effect/effect.module.ts
+++ src/modules/effect/effect.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { EFFECT_PERSISTENCE_REPOSITORY_TOKEN } from './application/ports/tokens';
+import { EffectFacade } from './application/facades/effect.facade';
+import {
+  EFFECT_FACADE_TOKEN,
+  EFFECT_PERSISTENCE_REPOSITORY_TOKEN,
+} from './application/ports/tokens';
+import { EffectOrmEntity } from './infrastructure/persistence/entities/effect-orm.entity';
 import { EffectPersistenceRepository } from './infrastructure/persistence/repositories/effect-persistence.repository';
 
 @Module({
+  imports: [CqrsModule, TypeOrmModule.forFeature([EffectOrmEntity])],
   providers: [
     {
       provide: EFFECT_PERSISTENCE_REPOSITORY_TOKEN,
       useClass: EffectPersistenceRepository,
     },
+    {
+      provide: EFFECT_FACADE_TOKEN,
+      useClass: EffectFacade,
+    },
   ],
+  exports: [EFFECT_FACADE_TOKEN],
 })
 export class EffectModule {}
