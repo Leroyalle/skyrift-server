a4856fb46a154f7e8bc77478e84d2db46e0f794b
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-24 21:58:41 +0300

    feat(mob-session): add getIterables reader method

--- src/realtime/mob-session/application/facades/mob-session.reader.ts
+++ src/realtime/mob-session/application/facades/mob-session.reader.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 
 import type { MobSessionRepositoryPort } from '../../domain/ports/in-memory-mob-session-repository.port';
+import type { MobSessionSnapshot } from '../../domain/types/mob-session.type';
 import type { MobSessionReaderPort } from '../ports/mob-session-reader.port';
 import { MOB_SESSION_REPOSITORY_TOKEN } from '../ports/tokens';
 
@@ -14,5 +15,12 @@ export class MobSessionReader implements MobSessionReaderPort {
   public getByLocationId(locationId: string) {
     const mobSessions = this.mobSessionRepository.getByLocationId(locationId);
     return mobSessions.map(session => session.toPublicSnapshot());
+  }
+
+  public *getIterable(): Iterable<MobSessionSnapshot> {
+    const sessions = this.mobSessionRepository.getIterable();
+    for (const session of sessions) {
+      yield session.toPublicSnapshot();
+    }
   }
 }
--- src/realtime/mob-session/application/ports/mob-session-reader.port.ts
+++ src/realtime/mob-session/application/ports/mob-session-reader.port.ts
@@ -2,4 +2,5 @@ import type { MobSessionSnapshot } from '../../domain/types/mob-session.type';
 
 export interface MobSessionReaderPort {
   getByLocationId(locationId: string): MobSessionSnapshot[];
+  getIterable(): Iterable<MobSessionSnapshot>;
 }
