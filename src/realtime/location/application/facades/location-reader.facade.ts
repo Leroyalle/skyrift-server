d8a63590a8ae05b3d5355319495ac4230eb68610
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-24 22:25:29 +0300

    feat(location): implement getByFilename reader method

--- src/realtime/location/application/facades/location-reader.facade.ts
+++ src/realtime/location/application/facades/location-reader.facade.ts
@@ -6,12 +6,16 @@ import type { LocationReaderPort } from '../ports/location-reader.port';
 import { LOCATION_REPOSITORY_TOKEN } from '../ports/tokens';
 
 @Injectable()
-export class LocationReaderQuery implements LocationReaderPort {
+export class LocationReaderFacade implements LocationReaderPort {
   constructor(
     @Inject(LOCATION_REPOSITORY_TOKEN) private readonly locationRepository: LocationRepositoryPort,
   ) {}
 
   public getById(id: ILocation['id']): ILocation | null {
     return this.locationRepository.get(id);
+  }
+
+  public getByFilename(filename: ILocation['filename']): ILocation | null {
+    return this.locationRepository.getByFilename(filename);
   }
 }
--- src/realtime/location/application/ports/location-reader.port.ts
+++ src/realtime/location/application/ports/location-reader.port.ts
@@ -2,4 +2,5 @@ import type { ILocation } from '../../domain/types/location.type';
 
 export interface LocationReaderPort {
   getById(id: ILocation['id']): ILocation | null;
+  getByFilename(filename: ILocation['filename']): ILocation | null;
 }
