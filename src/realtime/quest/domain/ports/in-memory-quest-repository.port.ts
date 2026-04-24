723ec7ad622047cab4cd10116b22d3f250f8bbf2
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-24 20:45:36 +0300

    feat(quest): implement getByGiverId repository method

--- src/realtime/quest/domain/ports/in-memory-quest-repository.port.ts
+++ src/realtime/quest/domain/ports/in-memory-quest-repository.port.ts
@@ -1,7 +1,8 @@
 import type { Quest } from 'src/modules/quest/domain/entities/quest.entity';
 
 export interface InMemoryQuestRepositoryPort {
-  getById(id: string): Quest | undefined;
+  getById(id: Quest['id']): Quest | undefined;
   save(quest: Quest): void;
-  remove(id: string): void;
+  remove(id: Quest['id'], giverId: Quest['giverId']): void;
+  getByGiverId(giverId: Quest['giverId']): Quest[];
 }
