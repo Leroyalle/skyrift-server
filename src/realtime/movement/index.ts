d6a588a86b0bc1a7956999b65977c3ba13f6ebc1
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-24 21:51:00 +0300

    feat(movement): implement module and register providers

--- src/realtime/movement/index.ts
+++ src/realtime/movement/index.ts
@@ -3,7 +3,12 @@ export {
   MOVEMENT_QUEUE_FACADE_TOKEN,
   PLAN_MOVEMENT_USE_CASE_TOKEN,
   APPROACH_TARGET_SERVICE_TOKEN,
+  PROCESS_MOVEMENT_TICK_TOKEN,
+  MOVEMENT_QUEUE_READER_TOKEN,
 } from './application/ports/tokens';
 export { type TDirection } from './domain/types/direction.type';
 export { PlanMovementUseCasePort } from './application/ports/plan-movement-use-case.port';
 export { ApproachTargetServicePort } from './application/ports/approach-target-service.port';
+export { ProcessMovementTickPort } from './application/ports/process-movement-tick.port';
+export { MovementQueueReaderPort } from './application/ports/movement-queue-reader.port';
+export { IMovementQueue } from './domain/types/movement-queue.type';
