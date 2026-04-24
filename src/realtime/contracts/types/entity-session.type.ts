85af869a9d3645cac92d208e6d8e437a486e7575
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-24 23:13:19 +0300

    feat(contracts): enhance entity session type by npc

--- src/realtime/contracts/types/entity-session.type.ts
+++ src/realtime/contracts/types/entity-session.type.ts
@@ -1,4 +1,5 @@
 import type { MobSessionSnapshot } from 'src/realtime/mob-session';
+import type { NpcSessionSnapshot } from 'src/realtime/npc-session';
 import type { PlayerSessionSnapshot } from 'src/realtime/player-session';
 
-export type TEntitySession = PlayerSessionSnapshot | MobSessionSnapshot;
+export type TEntitySession = PlayerSessionSnapshot | MobSessionSnapshot | NpcSessionSnapshot;
