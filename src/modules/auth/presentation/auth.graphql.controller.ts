f2e822529f63d9e9b4149ad35d945cfbdbd27fd3
Leroyalle <nikolajmelonov@yandex.ru>
2026-04-24 23:54:38 +0300

    feat(auth): implement graphql controller and add token data type

---
+++ src/modules/auth/presentation/auth.graphql.controller.ts
@@ -0,0 +1,90 @@
+import { Response } from 'express';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { AccessTokenGuard } from 'src/common/guards/access-token.guard';
+import { RefreshTokenGuard } from 'src/common/guards/refresh-token.guard';
+import { PayloadUser } from 'src/common/types/user-request.type';
+
+import { Inject, UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Context } from '@nestjs/graphql';
+
+import { LogoutPort } from '../application/ports/logout.port';
+import { RefreshPort } from '../application/ports/refresh.port';
+import { SignInPort } from '../application/ports/sign-in.port';
+import { SignUpPort } from '../application/ports/sign-up.port';
+import {
+  LOGOUT_USE_CASE_TOKEN,
+  REFRESH_USE_CASE_TOKEN,
+  SIGN_IN_USE_CASE_TOKEN,
+  SIGN_UP_USE_CASE_TOKEN,
+} from '../application/ports/tokens';
+
+import { LoginInput } from './dto/login.input';
+import { RegisterInput } from './dto/register.input';
+import { AuthModel } from './models/auth.model';
+import { LogoutModel } from './models/logout.model';
+
+@Resolver()
+export class AuthGraphqlController {
+  constructor(
+    @Inject(SIGN_IN_USE_CASE_TOKEN) private readonly signInUseCase: SignInPort,
+    @Inject(SIGN_UP_USE_CASE_TOKEN) private readonly signUpUseCase: SignUpPort,
+    @Inject(LOGOUT_USE_CASE_TOKEN) private readonly logoutUseCase: LogoutPort,
+    @Inject(REFRESH_USE_CASE_TOKEN) private readonly refreshUseCase: RefreshPort,
+  ) {}
+
+  @Mutation(() => AuthModel)
+  public signUp(@Args('signUpInput') signUpInput: RegisterInput) {
+    return this.signUpUseCase.execute(signUpInput);
+  }
+
+  @Mutation(() => AuthModel)
+  public async signIn(
+    @Args('signInInput') signInInput: LoginInput,
+    @Context() context: { res: Response },
+  ) {
+    const tokens = await this.signInUseCase.execute(signInInput);
+
+    context.res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    context.res.cookie('accessToken', tokens.accessToken, {
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 15,
+    });
+
+    return tokens;
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Mutation(() => LogoutModel)
+  public logout(@CurrentUser() user: PayloadUser) {
+    return this.logoutUseCase.execute(user.sub);
+  }
+
+  @UseGuards(RefreshTokenGuard)
+  @Mutation(() => AuthModel)
+  public async refreshTokens(
+    @CurrentUser() user: PayloadUser,
+    @Context() context: { res: Response },
+  ) {
+    console.log('REFRESH USR');
+    const tokens = await this.refreshUseCase.execute(user.sub, user.refreshToken);
+    context.res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    context.res.cookie('accessToken', tokens.accessToken, {
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 30,
+    });
+    return tokens;
+  }
+}
