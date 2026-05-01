dc4c0078d0a8b997106a7c256c0338e936b614aa
Leroyalle <nikolajmelonov@yandex.ru>
2026-05-02 01:55:49 +0300

    feat(seed): implement script

---
+++ src/infrastructure/seed/seed.script.ts
@@ -0,0 +1,23 @@
+import { AppModule } from 'src/app.module';
+import 'src/common/types/socket/socket';
+
+import { NestFactory } from '@nestjs/core';
+
+import { SeedService } from './seed.service';
+
+async function runSeed() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const seedService = app.get(SeedService);
+
+  try {
+    console.log('Running seed...');
+    await seedService.run();
+    console.log('Seed completed successfully');
+  } catch (error) {
+    console.error('Error running seed:', error);
+  } finally {
+    await app.close();
+  }
+}
+
+void runSeed();
