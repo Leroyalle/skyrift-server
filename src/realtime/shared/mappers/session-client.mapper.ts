7a2746cbcdfe2b9741b480e3dff84b6042d217a0
Leroyalle <nikolajmelonov@yandex.ru>
2026-05-07 02:49:37 +0300

    feat(session-client-mapper): concat with equipment

--- src/realtime/shared/mappers/session-client.mapper.ts
+++ src/realtime/shared/mappers/session-client.mapper.ts
@@ -1,21 +1,33 @@
+import { IEquipmentContainer } from 'src/realtime/container';
+import { EntityWithEquipment } from 'src/realtime/contracts/types/entity-with-equipment.type';
 import type { MobSessionSnapshot } from 'src/realtime/mob-session';
 import type { NpcSessionSnapshot } from 'src/realtime/npc-session';
 import type { ClientPlayerSession, PlayerSessionSnapshot } from 'src/realtime/player-session';
 
 export class SessionClientMapper {
-  public static mapPlayerSession = (session: PlayerSessionSnapshot): ClientPlayerSession => {
+  public static mapPlayerSession = (
+    session: PlayerSessionSnapshot,
+    equipment: IEquipmentContainer,
+  ): EntityWithEquipment<ClientPlayerSession> => {
     const { skillsById: _, ...cutSession } = session;
     return {
       ...cutSession,
       skills: Array.from(session.skillsById.values()),
+      equipment,
     };
   };
 
-  public static mapMobSession = (session: MobSessionSnapshot): MobSessionSnapshot => {
-    return session;
+  public static mapMobSession = (
+    session: MobSessionSnapshot,
+    equipment: IEquipmentContainer,
+  ): EntityWithEquipment<MobSessionSnapshot> => {
+    return { ...session, equipment };
   };
 
-  public static mapNpcSession = (session: NpcSessionSnapshot): NpcSessionSnapshot => {
-    return session;
+  public static mapNpcSession = (
+    session: NpcSessionSnapshot,
+    equipment: IEquipmentContainer,
+  ): EntityWithEquipment<NpcSessionSnapshot> => {
+    return { ...session, equipment };
   };
 }
