a3fe4f802fa4265e0cbb5c6d1fb4e411d5abd400
Leroyalle <nikolajmelonov@yandex.ru>
2026-05-07 03:28:37 +0300

    feat(contracts): update get initial data with equipment wrapper for creatures

--- src/realtime/contracts/types/game-initial-data.type.ts
+++ src/realtime/contracts/types/game-initial-data.type.ts
@@ -1,17 +1,18 @@
 import type { AoeZone } from 'src/realtime/combat';
-import type { IBagContainer, IEquipmentContainer } from 'src/realtime/container';
+import type { IBagContainer } from 'src/realtime/container';
 import type { ILocation } from 'src/realtime/location';
 import type { MobSessionSnapshot } from 'src/realtime/mob-session';
 import type { NpcSessionSnapshot } from 'src/realtime/npc-session';
 import type { ClientPlayerSession } from 'src/realtime/player-session';
 
+import { EntityWithEquipment } from './entity-with-equipment.type';
+
 export interface GameInitialData {
-  players: ClientPlayerSession[];
-  mobs: MobSessionSnapshot[];
+  players: EntityWithEquipment<ClientPlayerSession>[];
+  mobs: EntityWithEquipment<MobSessionSnapshot>[];
   aoeZones: AoeZone[];
-  npcs: NpcSessionSnapshot[];
+  npcs: EntityWithEquipment<NpcSessionSnapshot>[];
   location: ILocation;
-  player: ClientPlayerSession;
+  player: EntityWithEquipment<ClientPlayerSession>;
   bag: IBagContainer;
-  equipment: IEquipmentContainer;
 }
