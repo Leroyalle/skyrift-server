0c587888c8384abecf857c285a7d94b5faf1d22a
Leroyalle <nikolajmelonov@yandex.ru>
2026-05-07 03:14:45 +0300

    feat(location): add create location use case port

--- src/modules/location/application/use-cases/create-location.use-case.ts
+++ src/modules/location/application/use-cases/create-location.use-case.ts
@@ -2,10 +2,11 @@ import { Inject, Injectable } from '@nestjs/common';
 
 import type { LocationPersistenceRepositoryPort } from '../../domain/ports/location-persistence-repository.port';
 import type { ILocation } from '../../domain/types/location.type';
+import { CreateLocationUseCasePort } from '../ports/create-location-use-case.port';
 import { LOCATION_REPOSITORY_TOKEN } from '../ports/tokens';
 
 @Injectable()
-export class CreateLocationUseCase {
+export class CreateLocationUseCase implements CreateLocationUseCasePort {
   constructor(
     @Inject(LOCATION_REPOSITORY_TOKEN)
     private readonly locationRepository: LocationPersistenceRepositoryPort,
