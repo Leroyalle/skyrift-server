254864a07481a47616e88fc1e476b4ce30ed03bd
Leroyalle <nikolajmelonov@yandex.ru>
2026-05-07 03:35:32 +0300

    feat(presence): implement redis connection presence adapter, register providers

--- src/realtime/presence/index.ts
+++ src/realtime/presence/index.ts
@@ -1,2 +1,6 @@
-export { LOCATION_PRESENCE_ADAPTER_TOKEN } from './application/ports/tokens';
+export {
+  LOCATION_PRESENCE_ADAPTER_TOKEN,
+  CONNECTION_PRESENCE_ADAPTER_TOKEN,
+} from './application/ports/tokens';
 export { LocationPresenceAdapterPort } from './application/ports/location-presence-adapter.port';
+export { ConnectionPresenceAdapterPort } from './application/ports/connection-presence-adapter.port';
--- src/realtime/presence/presence.module.ts
+++ src/realtime/presence/presence.module.ts
@@ -2,12 +2,19 @@ import { RedisModule } from 'src/infrastructure/redis/redis.module';
 
 import { Module } from '@nestjs/common';
 
-import { LOCATION_PRESENCE_ADAPTER_TOKEN } from './application/ports/tokens';
+import {
+  CONNECTION_PRESENCE_ADAPTER_TOKEN,
+  LOCATION_PRESENCE_ADAPTER_TOKEN,
+} from './application/ports/tokens';
+import { RedisConnectionPresenceAdapter } from './infrastructure/membership/redis-connection-presence.adapter';
 import { RedisLocationPresenceAdapter } from './infrastructure/membership/redis-location-presence.adapter';
 
 @Module({
   imports: [RedisModule],
-  providers: [{ provide: LOCATION_PRESENCE_ADAPTER_TOKEN, useClass: RedisLocationPresenceAdapter }],
-  exports: [LOCATION_PRESENCE_ADAPTER_TOKEN],
+  providers: [
+    { provide: LOCATION_PRESENCE_ADAPTER_TOKEN, useClass: RedisLocationPresenceAdapter },
+    { provide: CONNECTION_PRESENCE_ADAPTER_TOKEN, useClass: RedisConnectionPresenceAdapter },
+  ],
+  exports: [LOCATION_PRESENCE_ADAPTER_TOKEN, CONNECTION_PRESENCE_ADAPTER_TOKEN],
 })
 export class PresenceModule {}
