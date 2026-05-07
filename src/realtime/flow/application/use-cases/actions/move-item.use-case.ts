e384cc5d6ccc2fe050a6739e9da7d71fc3e8de3a
Leroyalle <nikolajmelonov@yandex.ru>
2026-05-07 03:43:30 +0300

    feat(flow): emit item moved events in move item use case

--- src/realtime/flow/application/use-cases/actions/move-item.use-case.ts
+++ src/realtime/flow/application/use-cases/actions/move-item.use-case.ts
@@ -1,4 +1,7 @@
+import { SOCKET_ADAPTER_TOKEN, SocketAdapterPort } from 'src/infrastructure/ws';
 import { MOVE_ITEM_USE_CASE_TOKEN, type MoveItemUseCasePort } from 'src/realtime/container';
+import { RedisKeys } from 'src/realtime/contracts/constants/redis-keys.constant';
+import { ServerToClientEvents } from 'src/realtime/contracts/constants/socket-events.constant';
 import { ENTITY_RESOLVER_TOKEN, type EntityResolverPort } from 'src/realtime/entity-registry';
 
 import { Inject, Injectable } from '@nestjs/common';
@@ -15,6 +18,7 @@ export class MoveItemUseCase implements MoveItemPort {
   constructor(
     @Inject(MOVE_ITEM_USE_CASE_TOKEN) private readonly moveItemUseCase: MoveItemUseCasePort,
     @Inject(ENTITY_RESOLVER_TOKEN) private readonly entityResolver: EntityResolverPort,
+    @Inject(SOCKET_ADAPTER_TOKEN) private readonly socketAdapter: SocketAdapterPort,
   ) {}
 
   public moveFromBagToEquipment(payload: MoveFromBagToEquipmentPayload): MoveItemResult {
@@ -27,6 +31,18 @@ export class MoveItemUseCase implements MoveItemPort {
       itemId: payload.itemId,
     });
 
+    const result: MoveItemResult = {
+      changes,
+      entityRef: { id: character.id, type: character.type },
+    };
+
+    this.socketAdapter.sendTo(
+      // FIXME: сейчас шлется всем событие, нужно одному + всем смена экипировки, либо оставить
+      RedisKeys.Location + character.position.locationId,
+      ServerToClientEvents.ItemMoved,
+      result,
+    );
+
     return { changes, entityRef: { id: character.id, type: character.type } };
   }
 
@@ -38,6 +54,18 @@ export class MoveItemUseCase implements MoveItemPort {
       equipmentId: character.equipmentId,
       slot: payload.slot,
     });
+
+    const result: MoveItemResult = {
+      changes,
+      entityRef: { id: character.id, type: character.type },
+    };
+
+    this.socketAdapter.sendTo(
+      // FIXME: сейчас шлется всем событие, нужно одному + всем смена экипировки, либо оставить
+      RedisKeys.Location + character.position.locationId,
+      ServerToClientEvents.ItemMoved,
+      result,
+    );
 
     return { changes, entityRef: { id: character.id, type: character.type } };
   }
