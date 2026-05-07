2015c989758ca7d3ebc4b75f8253e9aa1dff8f56
Leroyalle <nikolajmelonov@yandex.ru>
2026-05-07 03:33:56 +0300

    feat(flow): add buildInitialWorldState use case port

--- src/realtime/flow/application/use-cases/initial-state/build-initial-world-state.use-case.ts
+++ src/realtime/flow/application/use-cases/initial-state/build-initial-world-state.use-case.ts
@@ -3,12 +3,13 @@ import type { GameInitialData } from 'src/realtime/contracts/types/game-initial-
 
 import { Inject, Injectable } from '@nestjs/common';
 
+import { BuildInitialWorldStatePort } from '../../ports/build-initial-world-state.port';
 import type { BuildLocationWorldStatePort } from '../../ports/build-location-world-state-use-case.port';
 import { BUILD_LOCATION_WORLD_STATE_USE_CASE_TOKEN } from '../../ports/tokens';
 import { InitializePlayerSessionUseCase } from '../session/initialize-player-session.use-case';
 
 @Injectable()
-export class BuildInitialWorldStateUseCase {
+export class BuildInitialWorldStateUseCase implements BuildInitialWorldStatePort {
   constructor(
     private readonly initializePlayerSessionUseCase: InitializePlayerSessionUseCase,
     @Inject(BUILD_LOCATION_WORLD_STATE_USE_CASE_TOKEN)
@@ -20,7 +21,7 @@ export class BuildInitialWorldStateUseCase {
 
     if (!playerResult) throw new Error('Player initialization failed');
 
-    const locationResult = this.buildLocationWorldStateUseCase.execute(
+    const locationResult = await this.buildLocationWorldStateUseCase.execute(
       playerResult.player.position.locationId,
     );
 
