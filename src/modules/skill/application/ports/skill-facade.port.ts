6f6a1cf077c1e54eeb9c0b47b90af85c3819a504
Leroyalle <nikolajmelonov@yandex.ru>
2026-05-07 03:19:17 +0300

    feat(skill): implement create and delete facade methods

--- src/modules/skill/application/ports/skill-facade.port.ts
+++ src/modules/skill/application/ports/skill-facade.port.ts
@@ -2,4 +2,6 @@ import type { ISkill } from '../../domain/types/skill.type';
 
 export interface SkillFacadePort {
   findByIds(ids: string[]): Promise<ISkill[]>;
+  create(payload: Omit<ISkill, 'id'>): Promise<ISkill>;
+  delete(id: string): Promise<void>;
 }
